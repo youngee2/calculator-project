0575981e70b66ee57bdc6789b2f8a53d60580687
youngee2 <jr3537@naver.com>
2025-04-17 12:21:07 +0900

    주석 정리 및 leve1 최종

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -5,13 +5,12 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
-    //사용자에게 입력받은 번호를 양의 정수인지 검사하는 변수(양의 정수(0 포함))
-    //static 메서드라서 뺄까 말까 고민중.. 작은 프로그램이라 상관 없을 것 같기도..
-    static boolean valueCheck(int num){
 
+    //사용자에게 입력받은 번호를 양의 정수인지 검사하는 메서드(양의 정수(0 포함))
+    static boolean valueCheck(int num){
         if (num < 0) {
             System.out.println("입력 가능한 값이 아닙니다. 양의 정수(0 포함) 입력해주세요.");
-            System.out.println("아무거나 입력하면 처음으로 돌아갑니다.");
+            System.out.println("next 입력하면 처음으로 돌아갑니다.");
             return true;
         }
         return false;
@@ -28,8 +27,9 @@ public class Calculator {
         //연산한 결과를 저장할 int형 result 변수 선언
         int result = 0;
 
-        //do-while을 선택한 이유
-        //한 번은 무조건 실행하기 때문에.
+        //한 번은 무조건 실행하기 때문에 do-while문 선택
+        //번호를 입력받은 후 valueCheck메서드로 범위에 해당하는 값인지 체크. 범위 포함 안되는 경우 continue로 프로그램 처음부터 실행.
+        //첫 번째 번호와 두번째 번호 같은 로직으로 진행.
         do{
             try {
                 System.out.print("첫 번째 번호를 입력: ");
@@ -43,18 +43,19 @@ public class Calculator {
                 if (valueCheck(numB)) {
                     continue;
                 }
-
+                
+                //사칙연산 기호를 입력받는 로직
+                //chatAt(0)으로 기호만 꺼내면 -1를 입력해도 -만 입력되기 때문에 그냥 넘어가는 현상 발생이 발생했기 때문에
+                //정규표현식으로 필터링. 만약 사칙연산 기호가 아닌 경우 프로그램 처음부터 실행.
                 System.out.print("사칙연산 기호를 입력하세요: ");
-                //-1를 입력해도 -만 입력되기 때문에 넘어가는 현상 발생.
                 String str = sc.next();
-
                 
                 //정규표현식으로 필터링 가능
                 // 사칙연산 기호를 받은 str 변수가 +,-,*,/가 아닌 경우 처음 로직으로 돌아감.
                 // 만약 올바른 사칙연산 기호를 입력받았다면 operation 변수에 저장.
                 if(!str.matches("[\\+\\-\\*/]")){
                     System.out.println("사칙연산 기호만 작성해주세요.");
-                    System.out.println("아무거나 입력하면 처음으로 돌아갑니다.");
+                    System.out.println("next 입력하면 처음으로 돌아갑니다.");
                     continue;
                 }else{
                     operator=str.charAt(0);
@@ -63,7 +64,6 @@ public class Calculator {
                 System.out.println("숫자를 제외한 값은 입력 불가. 처음으로 돌아갑니다.");
                 continue;
             }
-
 
             //switch문으로 해당 사칙연산 기호에 맞는 계산을 한 후, result변수에 저장하고 switch문을 빠져나감.
             switch (operator) {
@@ -84,26 +84,16 @@ public class Calculator {
                         continue;
                     };
                     break;
-
-                default: //사칙연산 기호가 아닐 경우 출력.
-                    System.out.println("사칙연산 기호가 옳지 않습니다. +, -, *, / 중 하나로 입력해주세요.");
-                    System.out.println("아무거나 입력하면 처음으로 돌아갑니다.");
-                    continue;
             }
 
-            //if문으로 연산 오류가 발생할 경우 해당 오류에 대한 내용을 정제하여 출력.
-            // 나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.
-
+            //계산된 값을 출력.
             System.out.println("결과: " + result);
-            //exit 값이 입력받으면 종료가 되나  계속 계산하려면 다른 값을 받아야 함.
-            // 2시간동안 고민하다가  담임매니저님께 가서 여쭤봤는데
-            // 요구사항에 exit 입력 시 종료, exit 아닐 시 반복되는게 튜터님이 원하는 요구사항 조건 충족하는 것 같다 그러셔서
-            // 유효성 검사를 하기 위해 exit-종료/next-진행으로 일단 하기로 결정.
-            // 예시에는 "더 계산하시겠습니까? (exit 입력 시 종료)라고 출력하라고 되어있는데, 사용자가 헷갈릴 것 같아서 계속 진행하려면 next 입력도 추가할 생각으로
-            // println문도 조금 수정해도 되냐고 여쭤봤더니 가능하다고 하셨다.
+
+            //exit 입력 시 종료하는 요구사항은 구현했지만 계속 진행하려면 next를 입력하라는 요구사항이 없었다.
+            //아무 값이나 입력하면 로직대로 진행되지만 사용자가 무엇을 입력해야 할 지 모를수도 있기 때문에 next를 입력하면 넘어간다고 표시했다.
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료 / 계속 진행하시려면 next 입력)");
 
-            //일단 exit 입력 시 종료/아무거나 입력시 계산 계속 진행 구현 O
+            //만약 입력받은 게 exit인 경우 반복 종료.
         }while(!sc.next().equals("exit"));
     }
 }
