5ca573df11abab943a50a414132b5702b5b2fe81
youngee2 <jr3537@naver.com>
2025-04-19 15:36:10 +0900

    로직 수정

--- src/com/example/calculator/v2/App.java
+++ src/com/example/calculator/v2/App.java
@@ -13,45 +13,44 @@ public class App {
         do {
             try {
                 System.out.print("첫 번째 번호를 입력: ");
-                calculator.numA = sc.nextInt();
-                if (calculator.valueCheck(calculator.numA)) {
+                int numA=sc.nextInt();
+                if (calculator.valueCheck(numA)) {
+                    calculator.setNumA(numA);
+                }else{
                     continue;
                 }
 
                 System.out.print("두 번째 번호를 입력: ");
-                calculator.numB = sc.nextInt();
-                if (calculator.valueCheck(calculator.numB)) {
+                int numB=sc.nextInt();
+                if (calculator.valueCheck(numB)) {
+                    calculator.setNumB(numB);
+                }else{
                     continue;
                 }
 
                 System.out.print("사칙연산 기호를 입력하세요: ");
                 String str = sc.next();
 
-
                 if (!str.matches("[\\+\\-\\*/]")) {
                     System.out.println("사칙연산 기호만 작성해주세요.");
                     System.out.println("next 입력하면 처음으로 돌아갑니다.");
                     continue;
                 } else {
-                    calculator.operator = str.charAt(0);
+                    calculator.setOperator(str.charAt(0));
                 }
             } catch (InputMismatchException e) {
                 System.out.println("숫자를 제외한 값은 입력 불가. 처음으로 돌아갑니다.");
                 continue;
             }
 
-            calculator.calculate(calculator.numA, calculator.numB, calculator.operator);
+            boolean resultCheck=calculator.calculate(calculator.getNumA(), calculator.getNumB(), calculator.getOperator());
 
-
-            System.out.println("결과: " + calculator.result);
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료 / 계속 진행하시려면 next 입력)");
+            if(resultCheck) {
+                System.out.println("결과: " + calculator.getResult());
+                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료 / 계속 진행하시려면 next 입력)");
+            }
 
             //만약 입력받은 게 exit인 경우 반복 종료.
         } while (!sc.next().equals("exit"));
-
-        //setter
-        calculator.setQueue(2,4);
-        //getter
-        System.out.println(calculator.getQueue());
     }
 }
--- src/com/example/calculator/v2/Calculator.java
+++ src/com/example/calculator/v2/Calculator.java
@@ -5,9 +5,10 @@ import java.util.Queue;
 
 public class Calculator {
 
-    int numA, numB;
-    char operator;
-    int result = 0;
+    private int numA;
+    private int numB;
+    private char operator;
+    private int result;
 
     //조건에 저장된 연산 결과들 중 가장 먼저 저장된 데이터를 삭제하는 기능을
     //가진 메서드를 구현하라고 하셔서 queue를 활용.
@@ -15,8 +16,18 @@ public class Calculator {
     private Queue<Integer> queueTemp = new LinkedList<>();
 
 
+    public boolean valueCheck(int num){
+        if (((num < 0) && (num<Integer.MAX_VALUE))) {
+            System.out.println("입력 가능한 값이 아닙니다. 양의 정수(0 포함) 입력해주세요.");
+            System.out.println("next 입력하면 처음으로 돌아갑니다.");
+            return false;
+        }else{
+            return true;
+        }
+    }
+
     //연산
-    public int calculate(int numA, int numB, char operator) {
+    public boolean calculate(int numA, int numB, char operator) {
         switch (operator) {
             case '+' -> result = numA + numB;
             case '-' -> result = numA - numB;
@@ -26,45 +37,69 @@ public class Calculator {
                     result = numA / numB;
                 } catch (ArithmeticException e) { //ArtimeticException 발생 시 아래 메세지 출력하고 처음부터 입력받음.
                     System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    return false;
                 }
             }
         }
         queue.add(result);
-        return result;
+        return true;
     }
-
 
     //가장 먼저 저장된 데이터 삭제하는 메서드
     public void removeResult() {
         queue.remove();
     }
 
-    //사용자에게 입력받은 번호를 양의 정수인지 검사하는 메서드(양의 정수(0 포함))
-    public boolean valueCheck(int num) {
-        if (num < 0) {
-            System.out.println("입력 가능한 값이 아닙니다. 양의 정수(0 포함) 입력해주세요.");
-            System.out.println("next 입력하면 처음으로 돌아갑니다.");
-            return true;
-        }
-        return false;
-    }
-
-
     public Queue getQueue() {
         return queue;
     }
 
-    int oldidx=0;
+
     public void setQueue(int idx,int num) {
-        while(!queue.isEmpty()){ //queue가 비어있지 않다면 반복
-        int value=queue.poll(); // 맨 처음 받은 값을 value에 저장
-        if(oldidx==idx){ //바꿀 인덱스가 같으면
-            queueTemp.add(num); //값을 덮어씌운다.
-        }else{
-            queueTemp.add(value); //만약 매개변수로 받은 바꿀 인덱스와 같지 않으면 덮어씌우지않는다.
+        int oldidx=0;
+        while((!queue.isEmpty())) { //queue가 비어있지 않고
+            if (idx <= queue.size()) {//setter로 받은 idx값이  현재 queue 사이즈보다 작거나 같다면
+                int value = queue.poll(); // 맨 처음 받은 값을 value에 저장
+                if (oldidx == idx) { //바꿀 인덱스가 같으면
+                    queueTemp.add(num); //값을 덮어씌운다.
+                } else {
+                    queueTemp.add(value); //만약 매개변수로 받은 바꿀 인덱스와 같지 않으면 덮어씌우지않는다.
+                }
+                oldidx++; //하나씩 증가하면서 인덱스를 증가.
+            }else{ //인덱스 범위를 벗어난다면
+                System.out.println("잘못된 인덱스입니다.");
+                break;
+            }
         }
-        oldidx++; //하나씩 증가하면서 인덱스를 증가.
-    }
         queue=queueTemp; //수정된 queueTemp를 queue에 저장.
+    }
+
+    public int getNumA() {
+        return numA;
+    }
+
+    public void setNumA(int numA) {
+            this.numA = numA;
+
+    }
+
+    public int getNumB() {
+        return numB;
+    }
+
+    public void setNumB(int numB) {
+            this.numB = numB;
+    }
+
+    public char getOperator() {
+        return operator;
+    }
+
+    public void setOperator(char operator) {
+        this.operator = operator;
+    }
+
+    public int getResult() {
+        return result;
     }
 }
